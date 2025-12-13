c1b78844efa6b78bda49744f1fe8cdbb2aca48c9
nisashap99 <nisashap99@gmail.com>
2025-12-13 11:49:10 +0530

    Docker

--- Docker-ec2/ec2.tf
+++ Docker-ec2/ec2.tf
@@ -2,11 +2,18 @@ resource "aws_instance" "this" {
   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
+
+  # 20GB is not enough
+  root_block_device {
+    volume_size = 50  # Set root volume size to 50GB
+    volume_type = "gp3"  # Use gp3 for better performance (optional)
+  }
+  user_data = file("docker.sh")
   tags = {
-    Name    = "Docker"
-    Purpose = "Docker-practice"
+    Name    = "docker"
   }
 }
+
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
