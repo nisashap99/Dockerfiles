27c1822898aafede6bbcb75aae29e38d41ae0b15
nisashap99 <nisashap99@gmail.com>
2025-12-23 12:57:02 +0530

    Docker

--- Docker-ec2/ec2.tf
+++ Docker-ec2/ec2.tf
@@ -19,9 +19,9 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
